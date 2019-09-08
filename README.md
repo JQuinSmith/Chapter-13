6d9fd6a0c06b286bf6ad21f5fb3cf02a7dae3e6f
QuinWithOneN <jquinsmyth@gmail.com>
2019-09-08 10:31:45 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Book 2 Chapter 13 Exercises and Practice
+###### Favorite Things Exercise complete.
---
+++ js/address.js
@@ -0,0 +1,22 @@
+// Some starter code
+const container = document.querySelector("#addressList")
+
+document.querySelector("#saveEntry").addEventListener("click", event => {
+    /*
+        Collect the user put by selecting the input fields, one
+        at a time, and accessing the `value` property
+    */
+    const favoriteItem = document.querySelector("#favoriteItem").value
+    const location = document.querySelector("#storeLocation").value
+
+    // Once you have collected all the values, update the DOM
+    // with some HTML
+    container.innerHTML += `
+        <section>
+            <h1>I can purchase ${favoriteItem} from ${location}.</h1>
+        </section>
+    `
+    // After clicking the submit button, having the value = an empty string resets the fields for new entries.
+        document.querySelector("#favoriteItem").value = ''
+        document.querySelector("#storeLocation").value = ''
+})
